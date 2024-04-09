ddb7eeb136bc1630cafa6b54f96fe240d606321d
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-04-09 13:50:09 +0300

    Haber analizi - Bölüm 2

--- Haber Analizi/Bölüm 2/BERTurk.py
+++ Haber Analizi/Bölüm 2/BERTurk.py
@@ -8,49 +8,41 @@ from sklearn.metrics import accuracy_score
 
 # Model ve Tokenizer yükle
 print("Model yükleniyor...")
-tokenizer = AutoTokenizer.from_pretrained(
-    "ytu-ce-cosmos/turkish-base-bert-uncased")
-model = AutoModel.from_pretrained("ytu-ce-cosmos/turkish-base-bert-uncased")
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
+model = AutoModel.from_pretrained("dbmdz/bert-base-turkish-cased")
 
 # GPU kullanılabilirse modeli aktar
-"""
-if torch.cuda.is_available():
-    model = model.cuda()
-    print("Model GPU'ya aktarıldı.")
-"""
-
+# Bu kısmı yoruma alıyorum, gerektiğinde aktive edebilirsiniz
+# if torch.cuda.is_available():
+#     model = model.cuda()
+#     print("Model GPU'ya aktarıldı.")
 
 def get_embeddings(text, index, total):
     print(f"İşlem: {index+1}/{total}...")
     inputs = tokenizer(text, return_tensors="pt",
                        padding=True, truncation=True, max_length=512)
-    # if torch.cuda.is_available():
-    #     inputs = {k: v.cuda() for k, v in inputs.items()}
     outputs = model(**inputs)
     embeddings = outputs.last_hidden_state.mean(dim=1).detach()
-    # if torch.cuda.is_available():
-    #     embeddings = embeddings.cpu()  # GPU'dan CPU'ya taşı
     return embeddings.numpy()
-
 
 def load_data(directory):
     texts, labels = [], []
-    label_dict = {"pozitif": 0, "negatif": 1, "tarafsiz": 2}
-    for label in label_dict:
-        dir_path = os.path.join(directory, label)
+    categories = ["ekonomi", "magazin", "siyaset", "saglik", "spor"]
+    label_dict = {category: i for i, category in enumerate(categories)}
+    for category in categories:
+        dir_path = os.path.join(directory, category)
         for filename in os.listdir(dir_path):
             file_path = os.path.join(dir_path, filename)
             with open(file_path, 'r', encoding='ISO-8859-1') as file:
                 texts.append(file.read())
-                labels.append(label_dict[label])
+                labels.append(label_dict[category])
     return texts, labels
-
 
 # Veri kümesini yükleyin ve etiketlerini ayarlayın
 print("Veri kümesi yükleniyor...")
-texts, labels = load_data('../../datasets/sentiment')
+texts, labels = load_data('../../datasets/news')
 
-# Toplam soru sayısını hesapla
+# Toplam metin sayısını hesapla
 total_texts = len(texts)
 
 # Temsilleri elde et
@@ -64,9 +56,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Belirli cümleler için temsilcileri (embeddings) al
 reference_texts = [
-    "Bu metin olumlu ifadeler içermektedir.",
-    "Bu metin olumsuz ifadeler içermektedir.",
-    "Bu metin tarafsız ifadeler içermektedir."
+    "Bu metin ekonomi türündedir.",
+    "Bu metin sağlık türündedir.",
+    "Bu metin siyaset türündedir.",
+    "Bu metin magazin türündedir.",
+    "Bu metin spor türündedir."
 ]
 reference_embeddings = np.vstack([get_embeddings(text, idx, len(reference_texts))
                                   for idx, text in enumerate(reference_texts)])
